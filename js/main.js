3b86ae73fa10993c6db9fe6260bdaaad65f2d091
PROLOVE <proLove0212@gmail.com>
2023-01-23 19:57:55 +0300

    nav js fix

--- js/main.js
+++ js/main.js
@@ -16,12 +16,20 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link')
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active')
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav_active')
     });
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1){
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active');
+            })
+        }
+    }
 }());
 
 // Scroll to anchors
